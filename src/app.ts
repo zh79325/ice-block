cc2aa956d44706c4b6adc8a0da3ede905b2a488b
zzh250578 <zzh250578@alibaba-inc.com>
2025-04-14 11:55:57 +0800

    publicPath

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,8 @@
 import { defineAppConfig } from 'ice';
 // App config, see https://v3.ice.work/docs/guide/basic/app
 export default defineAppConfig(() => ({
+    router: {
+        type: 'hash',
+    },
 }));
 
